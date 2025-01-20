928f282cc3fa5ac14eafcc92e72ebef5e0a23d03
Nabi <matibag.abi@gmail.com>
2025-01-20 23:30:09 +0800

    Add button for add time

--- src/components/AppSidebar.tsx
+++ src/components/AppSidebar.tsx
@@ -1,7 +1,8 @@
-import { Calendar, Clipboard, ChartLine, AlarmClockCheck } from "lucide-react"
+import { Calendar, Clipboard, ChartLine, AlarmClockCheck, Github } from "lucide-react"
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
@@ -9,22 +10,23 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar"
+import Link from "next/link"
 
 // Menu items.
 const items = [
   {
     title: "Daily",
-    url: "#",
+    href: "/",
     icon: Calendar,
   },
   {
     title: "Monthly",
-    url: "#",
+    href: "/",
     icon: Clipboard,
   },
   {
     title: "Yearly",
-    url: "#",
+    href: "/",
     icon: ChartLine,
   }
 ]
@@ -34,12 +36,14 @@ export function AppSidebar() {
     <Sidebar>
       <SidebarContent>
         <div className="p-2">
-          <button className="flex flex-row items-center gap-2 p-2 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground w-full rounded-md">
-            <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground"><AlarmClockCheck size={20} /></div>
-            <div>
-              <span className="font-semibold">TimeWise</span>
-            </div>
-          </button>
+          <Link href="/">
+            <button className="flex flex-row items-center gap-2 p-2 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground w-full rounded-md">
+              <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground"><AlarmClockCheck size={20} /></div>
+              <div>
+                <span className="font-semibold">TimeWise</span>
+              </div>
+            </button>
+          </Link>
         </div>
         <SidebarGroup>
           <SidebarGroupLabel>Pages</SidebarGroupLabel>
@@ -48,10 +52,10 @@ export function AppSidebar() {
               {items.map((item) => (
                 <SidebarMenuItem key={item.title}>
                   <SidebarMenuButton asChild>
-                    <a href={item.url}>
+                    <Link href={item.href}>
                       <item.icon />
                       <span>{item.title}</span>
-                    </a>
+                    </Link>
                   </SidebarMenuButton>
                 </SidebarMenuItem>
               ))}
@@ -59,6 +63,16 @@ export function AppSidebar() {
           </SidebarGroupContent>
         </SidebarGroup>
       </SidebarContent>
+      <SidebarFooter>
+        <Link href="https://github.com/nabi-cloud" target="new">
+          <button className="flex flex-row items-center gap-2 p-2 hover:bg-sidebar-accent hover:text-sidebar-accent-foreground w-full rounded-md">
+            <div className="flex aspect-square size-5 items-center justify-center rounded bg-sidebar-primary text-sidebar-primary-foreground"><Github size={12} /></div>
+            <div>
+              <span className="text-xs text-gray-400">Nabicloud | 2025</span>
+            </div>
+          </button>
+        </Link>
+      </SidebarFooter>
     </Sidebar>
   )
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
 import "@/styles/globals.css";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { SidebarProvider, SidebarTrigger, SidebarInset } from "@/components/ui/sidebar"
+import { Separator } from "@/components/ui/separator"
 import { AppSidebar } from "@/components/AppSidebar"
 import { Geist } from "next/font/google";
 import { AppProps } from "next/app";
+import Head from "next/head";
+import { Button } from "@/components/ui/button";
+import { Plus, Moon } from 'lucide-react';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -12,16 +16,29 @@ const geistSans = Geist({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SidebarProvider className={`${geistSans.variable} min-h-screen font-[family-name:var(--font-geist-sans)]`}>
-      <div className="relative flex min-h-screen w-full">
-        <AppSidebar />
-        <main className="flex flex-col flex-1 p-4">
-          <header className="flex flex-row items-center gap-2 pb-4 w-full">
-            <SidebarTrigger />
-            <div data-orientation="vertical" role="none" className="shrink-0 bg-border w-[1px] mr-2 h-4"></div>
-          </header>
+      <Head>
+        <title>Ministry Time Tracker</title>
+        <meta name="description" content="Ministry time tracker webapp" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator orientation="vertical" className="mr-2 h-6" />
+            <Button variant="ghost">
+              <Moon size={18} />
+            </Button>
+            <Button className="h-8">
+              <Plus /> Add Time
+            </Button>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4 p-4">
           <Component {...pageProps} />
-        </main>
-      </div>
+        </div>
+      </SidebarInset>
     </SidebarProvider>
   );
 }
